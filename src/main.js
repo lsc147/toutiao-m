be4066144f6d3e7f6fc0e5b6356129d42100f00f
lsc <2302492042@qq.com>
2022-08-05 10:23:06 +0800

    引入vant组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 import '@/styles/index.less'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+
+// import { createApp } from 'vue';
+// import Vant from 'vant';
+// import 'vant/lib/index.css';
+
+// const app = createApp();
+// app.use(Vant);
 
 Vue.config.productionTip = false
 
